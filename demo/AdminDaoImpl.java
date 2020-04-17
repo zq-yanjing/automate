2fe8d88a55b3b2fc66cb52ea93a2cb4aca12b813
zq <zq@163.com>
2020-04-17 17:27:40 +0800

    zq-yanjing xiugai

---
+++ demo/AdminDaoImpl.java
@@ -0,0 +1,9 @@
+public class AdminDaoImpl{
+
+public int login(master做的login){}
+public int register(){}
+public int add(){}
+public List<>findall(){}
+public int update(){}
+public int delete(){}
+}
---
+++ demo/user.java
@@ -0,0 +1,7 @@
+public class User{
+
+private int id;
+private String username;
+private String password;
+private String sex;
+}
